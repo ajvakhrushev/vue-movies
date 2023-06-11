ec933d880da36de3f2bcc5d3b7fc4cd4209e55e7
Andrey Vakhrushev <a.j.vakhrushev@outlook.com>
2023-06-11 19:36:56 +0300

    added movies filter base for component

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+// import MdButton from "vue-material/src/components/MdButton/MdButton.vue";
 import "modern-css-reset/dist/reset.min.css";
+import "vue-material/dist/vue-material.min.css";
 import "./styles.scss";
 
 createApp(App).use(store).use(router).mount("#app");
